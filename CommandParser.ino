12bd64a06714185211a7cd94a8bed45c56d1df25
Pedro Lacerda <phenrique.lacerda1@gmail.com>
2019-06-19 08:39:16 -0300

    Quick code Refactor

--- CommandParser.ino
+++ CommandParser.ino
@@ -6,72 +6,91 @@ Bancada bancada;
 boolean shouldLoop = false;
 Expression loopExpression;
 
-void setup() {
-  pinMode(6, OUTPUT);
-  
+void setup()
+{
+	pinMode(6, OUTPUT);
+
 	Serial.begin(9600);
-	
+
 	delay(1000);
 	Serial.println("Starting...");
 }
 
-void loop() {
-	if(shouldLoop){
+void loop()
+{
+	if (shouldLoop)
+	{
 		doLoop();
 	}
 
-	if(Serial.available()){
+	if (Serial.available())
+	{
 		getComando();
 	}
 }
 
-void doLoop(){
+void doLoop()
+{
 	bancada.executaExpression(&loopExpression, 1);
 }
 
-void getComando(){
-	//Serial.println("Interpretando comando...");
+void getComando()
+{
 
 	CommandParser::parseCommand(Serial.readString(), &cmdBuffer);
-
-	//Serial.print(cmdBuffer.name);
-	//cmdBuffer.printArgs();
 	executeCommand(&cmdBuffer);
 }
 
-void executeCommand(Command* cmd){
+void executeCommand(Command *cmd)
+{
 	//addAtuadorDigital(nome, porta, estadoInicial, tempoAtuacao)
-	if(cmd->name == "addAtuadorDigital" && cmd->argCount == 4){
-		bancada.adicionaAtuador(new AtuadorDigital (cmd->args[0],
-													cmd->args[1].toInt(),
-													cmd->args[2] == "1",
-													cmd->args[3].toInt()));		
-	}else if(cmd->name == "addServo" && cmd->argCount == 4){
+	if (cmd->name == "addAtuadorDigital" && cmd->argCount == 4)
+	{
+		bancada.adicionaAtuador(new AtuadorDigital(cmd->args[0],
+																							 cmd->args[1].toInt(),
+																							 cmd->args[2] == "1",
+																							 cmd->args[3].toInt()));
+	}
+	else if (cmd->name == "addServo" && cmd->argCount == 4)
+	{
 		//addServo(nome, porta, estadoInicial, tempoAtuacao)
-		bancada.adicionaAtuador(new Servo (cmd->args[0],
-										   cmd->args[1].toInt(),
-										   cmd->args[2] == "1",
-										   cmd->args[3].toInt()));
-	}else if(cmd->name == "listAtuadores"){
+		bancada.adicionaAtuador(new Servo(cmd->args[0],
+																			cmd->args[1].toInt(),
+																			cmd->args[2] == "1",
+																			cmd->args[3].toInt()));
+	}
+	else if (cmd->name == "listAtuadores")
+	{
 		//listAtuadores
 		bancada.listaAtuadores();
-	}else if(cmd->name == "removeAtuador" && cmd->argCount == 1){
+	}
+	else if (cmd->name == "removeAtuador" && cmd->argCount == 1)
+	{
 		//removeAtuador(nome)
 		bancada.removeAtuador(cmd->args[0]);
-	}else if((cmd->name == "loop" || cmd->name == "execute") && !shouldLoop && cmd->argCount >= 1){
-    	//execute(expression) | loop(expression, repetitions) | loop(expression)
+	}
+	else if ((cmd->name == "loop" || cmd->name == "execute") && !shouldLoop && cmd->argCount >= 1)
+	{
+		//execute(expression) | loop(expression, repetitions) | loop(expression)
 		CommandParser::parseExpression(cmd->args[0], &loopExpression);
-    	if(cmd->argCount == 2 || (cmd->argCount == 1 && cmd->name == "execute")){
-    		bancada.executaExpression(&loopExpression, cmd->name == "loop" ? cmd->args[1].toInt() : 1);
-    	}else{
-    		shouldLoop = true;
-    		doLoop();
-    	}
-    }else if(cmd->name == "atuar" && !shouldLoop && cmd->argCount == 2){
-    	//atuar(nome, estado)
-    	bancada.atuar(cmd->args[0],
-                	  cmd->args[1].toInt());
-	}else if(cmd->name == "help" && !shouldLoop){
+		if (cmd->argCount == 2 || (cmd->argCount == 1 && cmd->name == "execute"))
+		{
+			bancada.executaExpression(&loopExpression, cmd->name == "loop" ? cmd->args[1].toInt() : 1);
+		}
+		else
+		{
+			shouldLoop = true;
+			doLoop();
+		}
+	}
+	else if (cmd->name == "atuar" && !shouldLoop && cmd->argCount == 2)
+	{
+		//atuar(nome, estado)
+		bancada.atuar(cmd->args[0],
+									cmd->args[1].toInt());
+	}
+	else if (cmd->name == "help" && !shouldLoop)
+	{
 		//help
 		Serial.println("Comandos:\n");
 		Serial.println("\n- addAtuadorDigital([nome do atuador], [porta], [estado inicial], [tempo de atuação])\n   Adiciona um atuador digital à bancada");
@@ -82,15 +101,15 @@ void executeCommand(Command* cmd){
 		Serial.println("\n- loop([expressão], [número de repetições])\n   Executa uma expressão um número determinado de vezes");
 		Serial.println("\n- loop([expressão])\n   Executa uma expressão até que seja interrompido");
 		Serial.println("\n- parar()\n   Interrompe a execução de um loop");
-    }else if(cmd->name == "parar" && shouldLoop){
+	}
+	else if (cmd->name == "parar" && shouldLoop)
+	{
 		//parar
 		Serial.println("Parando loop");
-    	shouldLoop = false;
-    }else{
-    	Serial.println("Comando não reconhecido");
-    }
-    
+		shouldLoop = false;
+	}
+	else
+	{
+		Serial.println("Comando não reconhecido");
+	}
 }
-
-
-
